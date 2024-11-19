2a61a4e5224736ee77dfdd86767fca8c3dd0b791
haxxTom <tomas.bilek.2006@gmail.com>
2024-11-19 19:10:03 +0100

    first commit

---
+++ dropdown.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var i = 0;
+var txt = 'Aplikace a bezpečnost na míru pro vaši firmu';
+var speed = 20;
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    typeWriter();
+  
+});
+
+function typeWriter(){
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+  
